bff67d1b4b79abba6401185c7b425b14a306405c
Brandon Harrell <branh0913@live.com>
2020-02-02 12:51:42 -0500

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/brharrelldev/crytoTrader
+
+go 1.13
+
+require github.com/urfave/cli v1.22.2
---
+++ integration/integration.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"github.com/brharrelldev/crytoTrader/config"
+	"github.com/brharrelldev/crytoTrader/general"
+	"log"
+)
+
+func main() {
+
+	c := config.Config{
+		BaseURL: "https://api.binance.com",
+	}
+
+	ping, err := general.NewGeneralAPI(&c)
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	resp, err := ping.GetPing()
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	pingJson, err := resp.ToJson()
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	fmt.Println(pingJson)
+
+	serverTime, err := ping.CheckServiceTime()
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	serverTimeJson, err := serverTime.ToJson()
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	fmt.Println(serverTimeJson)
+
+
+}
