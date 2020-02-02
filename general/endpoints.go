a7af303c91d17391edc15256dab3582c96543292
Brandon Harrell <branh0913@live.com>
2020-02-02 16:12:49 -0500

    getting rid of linting errors

--- general/endpoints.go
+++ general/endpoints.go
@@ -35,7 +35,7 @@ func NewGeneralAPI(c *config.Config) (*General, error) {
 
 }
 
-// will be used elsewhere in application
+//GetPing() (*pingResponse, error)
 func (g *General) GetPing() (*pingResponse, error) { //test comment
 	// will be used elsewhere in application
 
